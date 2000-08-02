9be40bc2afdc68a5472aa0409c1a71bf935bd3cc
bagder <bagder>
2000-08-02 07:16:57 +0000

    moved the #define strequal() since they're now true functions in libcurl

--- src/setup.h
+++ src/setup.h
@@ -31,8 +31,8 @@
  * 	http://curl.haxx.nu
  *
  * $Source: /cvsroot/curl/curl/src/setup.h,v $
- * $Revision: 1.3 $
- * $Date: 2000-03-19 19:55:02 $
+ * $Revision: 1.4 $
+ * $Date: 2000-08-02 07:16:57 $
  * $Author: bagder $
  * $State: Exp $
  * $Locker:  $
@@ -61,20 +61,8 @@
 #define OS "unknown"
 #endif
 
-#ifdef HAVE_STRCASECMP
-#define strnequal(x,y,z) !(strncasecmp)(x,y,z)
-#define strequal(x,y) !(strcasecmp)(x,y)
-
-/* this is for "-ansi -Wall -pedantic" to stop complaining! */
-extern int (strcasecmp)(const char *s1, const char *s2);
-extern int (strncasecmp)(const char *s1, const char *s2, size_t n);
 #ifndef fileno /* sunos 4 have this as a macro! */
 int fileno( FILE *stream);
-#endif
-
-#else
-#define strnequal(x,y,z) !strnicmp(x,y,z)
-#define strequal(x,y) !stricmp(x,y)
 #endif
 
 #ifdef WIN32
