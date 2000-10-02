3e7a44d3109a635b51e002bb67a0f593ff5a7bcd
bagder <bagder>
2000-10-02 06:29:39 +0000

    adjusted for curl_easy_getinfo

--- include/curl/easy.h
+++ include/curl/easy.h
@@ -31,8 +31,8 @@
  * 	http://curl.haxx.nu
  *
  * $Source: /cvsroot/curl/curl/include/curl/easy.h,v $
- * $Revision: 1.2 $
- * $Date: 2000-09-07 11:34:08 $
+ * $Revision: 1.3 $
+ * $Date: 2000-10-02 06:29:39 $
  * $Author: bagder $
  * $State: Exp $
  * $Locker:  $
@@ -47,6 +47,21 @@ CURL *curl_easy_init(void);
 CURLcode curl_easy_setopt(CURL *curl, CURLoption option, ...);
 CURLcode curl_easy_perform(CURL *curl);
 void curl_easy_cleanup(CURL *curl);
+
+/*
+ * NAME curl_easy_getinfo()
+ *
+ * DESCRIPTION
+ *
+ * Request internal information from the curl session with this function.  The
+ * third argument MUST be a pointer to a long, a pointer to a char * or a
+ * pointer to a double (as the documentation describes elsewhere).  The data
+ * pointed to will be filled in accordingly and can be relied upon only if the
+ * function returns CURLE_OK.  This function is intended to get used *AFTER* a
+ * performed transfer, all results from this function are undefined until the
+ * transfer is completed.
+ */
+CURLcode curl_easy_getinfo(CURL *curl, CURLINFO info, ...);
 
 #ifdef  __cplusplus
 }
