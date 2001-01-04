57ff73d886d258ced88f5250a9799d1cb682739f
bagder <bagder>
2001-01-04 12:27:04 +0000

    updated license text in headers

--- lib/mprintf.c
+++ lib/mprintf.c
@@ -1,8 +1,17 @@
 /****************************************************************************
  *
- * $Id: mprintf.c,v 1.5 2000-11-21 13:18:30 bagder Exp $
+ * $Id: mprintf.c,v 1.6 2001-01-04 12:27:04 bagder Exp $
  *
  *************************************************************************
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE AUTHORS AND
+ * CONTRIBUTORS ACCEPT NO RESPONSIBILITY IN ANY CONCEIVABLE MANNER.
  *
  * Purpose:
  *  A merge of Bjorn Reese's format() function and Daniel's dsprintf()
@@ -83,7 +92,7 @@
  *
  ****************************************************************************/
 
-static const char rcsid[] = "@(#)$Id: mprintf.c,v 1.5 2000-11-21 13:18:30 bagder Exp $";
+static const char rcsid[] = "@(#)$Id: mprintf.c,v 1.6 2001-01-04 12:27:04 bagder Exp $";
 
 /*
  * To test:
