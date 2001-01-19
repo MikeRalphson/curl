2a49fad0209c84366acba79289a6587ba6eaa9c6
bagder <bagder>
2001-01-19 09:38:48 +0000

    version 7.6-pre3

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: curl.h,v 1.43 2001-01-11 09:29:30 bagder Exp $
+ * $Id: curl.h,v 1.44 2001-01-19 09:38:48 bagder Exp $
  *****************************************************************************/
 
 #include <stdio.h>
@@ -456,7 +456,7 @@ char *curl_getenv(char *variable);
 char *curl_version(void);
 
 /* This is the version number */
-#define LIBCURL_VERSION "7.6-pre2"
+#define LIBCURL_VERSION "7.6-pre3"
 #define LIBCURL_VERSION_NUM 0x070600
 
 /* linked-list structure for the CURLOPT_QUOTE option (and other) */
