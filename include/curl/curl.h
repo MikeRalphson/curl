02020466ce9d677a76e8233189c0598b3d2730ae
bagder <bagder>
2001-03-19 08:42:00 +0000

    7.7-beta5 commit

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: curl.h,v 1.67 2001-03-15 14:37:41 bagder Exp $
+ * $Id: curl.h,v 1.68 2001-03-19 08:42:00 bagder Exp $
  *****************************************************************************/
 
 #include <stdio.h>
@@ -476,7 +476,7 @@ char *curl_getenv(char *variable);
 char *curl_version(void);
 
 /* This is the version number */
-#define LIBCURL_VERSION "7.7-beta3"
+#define LIBCURL_VERSION "7.7-beta5"
 #define LIBCURL_VERSION_NUM 0x070700
 
 /* linked-list structure for the CURLOPT_QUOTE option (and other) */
