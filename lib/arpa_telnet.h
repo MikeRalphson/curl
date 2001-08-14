b8bedfe267953e9788086afeeb058ce0b40b7316
bagder <bagder>
2001-08-14 08:16:53 +0000

    Added 'const' to the string arrays

--- lib/arpa_telnet.h
+++ lib/arpa_telnet.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: arpa_telnet.h,v 1.3 2001-08-03 15:11:28 bagder Exp $
+ * $Id: arpa_telnet.h,v 1.4 2001-08-14 08:16:53 bagder Exp $
  *****************************************************************************/
 
 /*
@@ -39,7 +39,7 @@
 /*
  * The telnet options represented as strings
  */
-static char *telnetoptions[]=
+static const char *telnetoptions[]=
 {
   "BINARY",      "ECHO",           "RCP",           "SUPPRESS GO AHEAD",
   "NAME",        "STATUS"          "TIMING MARK",   "RCTE",
@@ -78,7 +78,7 @@ static char *telnetoptions[]=
 /*
  * Then those numbers represented as strings:
  */
-static char *telnetcmds[]=
+static const char *telnetcmds[]=
 {
   "EOF",  "SUSP",  "ABORT", "EOR",  "SE",
   "NOP",  "DMARK", "BRK",   "IP",   "AO",
