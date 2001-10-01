75756ae9161e2130b6d6c9d543f740f54e4bfeda
bagder <bagder>
2001-10-01 22:50:20 +0000

    added comment to the tvdiff

--- lib/timeval.c
+++ lib/timeval.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: timeval.c,v 1.7 2001-09-07 04:01:33 bumblebury Exp $
+ * $Id: timeval.c,v 1.8 2001-10-01 22:50:20 bagder Exp $
  *****************************************************************************/
 
 #ifdef WIN32
@@ -65,6 +65,10 @@ struct timeval Curl_tvnow (void)
  return now;
 }
 
+/*
+ * Make sure that the first argument is the more recent time, as otherwise
+ * we'll get a weird negative time-diff back...
+ */
 double Curl_tvdiff (struct timeval t1, struct timeval t2)
 {
  return (double)(t1.tv_sec - t2.tv_sec) + ((t1.tv_usec-t2.tv_usec)/1000000.0);
