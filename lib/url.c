2123648b37cb2a74a0904f49b1b5fdf083b15807
bagder <bagder>
2001-10-02 11:26:53 +0000

    IPv6 adjustments, connect()ing to bad ports still don't work properly for IPv6

--- lib/url.c
+++ lib/url.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: url.c,v 1.157 2001-10-02 09:40:06 bagder Exp $
+ * $Id: url.c,v 1.158 2001-10-02 11:26:53 bagder Exp $
  *****************************************************************************/
 
 /* -- WIN32 approved -- */
@@ -1949,7 +1949,7 @@ static CURLcode CreateConnection(struct SessionHandle *data,
     if(!conn->hostaddr) {
       /* it might already be set if reusing a connection */
       conn->hostaddr = Curl_getaddrinfo(data, conn->name, conn->port,
-                                  &conn->hostent_buf);
+                                        &conn->hostent_buf);
     }
     if(!conn->hostaddr) {
       failf(data, "Couldn't resolve host '%s'", conn->name);
@@ -1963,7 +1963,7 @@ static CURLcode CreateConnection(struct SessionHandle *data,
     /* resolve proxy */
     /* it might already be set if reusing a connection */
     conn->hostaddr = Curl_getaddrinfo(data, conn->proxyhost, conn->port,
-                                &conn->hostent_buf);
+                                      &conn->hostent_buf);
 
     if(!conn->hostaddr) {
       failf(data, "Couldn't resolve proxy '%s'", conn->proxyhost);
@@ -2040,7 +2040,7 @@ static CURLcode CreateConnection(struct SessionHandle *data,
 #else
     const int niflags = NI_NUMERICHOST;
 #endif
-    struct addrinfo *ai = conn->ai;
+    struct addrinfo *ai = conn->serv_addr;
 
     if (getnameinfo(ai->ai_addr, ai->ai_addrlen, hbuf, sizeof(hbuf), NULL, 0,
 	niflags)) {
