ca1f8013d89ba95e863f21d98b091481017bf044
bagder <bagder>
2001-11-27 00:50:52 +0000

    commented out the EPSV support

--- lib/ftp.c
+++ lib/ftp.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: ftp.c,v 1.111 2001-11-27 00:48:45 bagder Exp $
+ * $Id: ftp.c,v 1.112 2001-11-27 00:50:52 bagder Exp $
  *****************************************************************************/
 
 #include "setup.h"
@@ -1343,8 +1343,8 @@ CURLcode ftp_use_pasv(struct connectdata *conn)
        we're already connected to, as then we can skip the name function
        call below, in similar style that we do for the EPSV reply */
   }
-#if 1
-  else if (strcmp(mode[modeoff], "EPSV") == 0) {
+#if 0
+  else if (229 == results[modeoff]) {
     char *ptr = strchr(buf, '(');
     if(ptr) {
       unsigned int num;
