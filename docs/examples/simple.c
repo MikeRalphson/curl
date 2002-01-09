3d49aa442c53d4051f14e4bf86d5c0d64ddff49e
bagder <bagder>
2002-01-09 13:22:03 +0000

    Cleaned up this example to make it even simpler.

--- docs/examples/simple.c
+++ docs/examples/simple.c
@@ -5,31 +5,21 @@
  *                            | (__| |_| |  _ <| |___ 
  *                             \___|\___/|_| \_\_____|
  *
- * $Id: simple.c,v 1.2 2001-02-20 13:56:39 bagder Exp $
+ * $Id: simple.c,v 1.3 2002-01-09 13:22:03 bagder Exp $
  */
 
 #include <stdio.h>
-
 #include <curl/curl.h>
-#include <curl/types.h>
-#include <curl/easy.h>
-
-/* to make this work under windows, use the win32-functions from the
-   win32socket.c file as well */
 
 int main(int argc, char **argv)
 {
   CURL *curl;
   CURLcode res;
-  FILE *headerfile;
-
-  headerfile = fopen("dumpit", "w");
 
   curl = curl_easy_init();
   if(curl) {
     /* what call to write: */
     curl_easy_setopt(curl, CURLOPT_URL, "curl.haxx.se");
-    curl_easy_setopt(curl, CURLOPT_WRITEHEADER, headerfile);
     res = curl_easy_perform(curl);
 
     /* always cleanup */
