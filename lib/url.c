b88c1dc183ecd3774af82d824480aaf305edcfde
bagder <bagder>
2002-03-14 14:37:16 +0000

    CURLOPT_POST deserved a new comment with the new POST-by-callback support

--- lib/url.c
+++ lib/url.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: url.c,v 1.193 2002-03-13 13:10:52 bagder Exp $
+ * $Id: url.c,v 1.194 2002-03-14 14:37:16 bagder Exp $
  *****************************************************************************/
 
 /* -- WIN32 approved -- */
@@ -634,8 +634,9 @@ CURLcode Curl_setopt(struct SessionHandle *data, CURLoption option, ...)
     data->set.use_port = va_arg(param, long);
     break;
   case CURLOPT_POST:
-    /* Does this option serve a purpose anymore? */
-
+    /* Does this option serve a purpose anymore? Yes it does, when
+       CURLOPT_POSTFIELDS isn't used and the POST data is read off the
+       callback! */
     if(va_arg(param, long))
       data->set.httpreq = HTTPREQ_POST;
     break;
