eac6a01db66582c1e804d22081b7e2c378d90164
bagder <bagder>
2002-04-02 09:33:38 +0000

    Clarence Gardner pointed out the not-taken-care-of return codes from SSL_read

--- lib/sendf.c
+++ lib/sendf.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: sendf.c,v 1.46 2002-03-19 07:54:55 bagder Exp $
+ * $Id: sendf.c,v 1.47 2002-04-02 09:33:38 bagder Exp $
  *****************************************************************************/
 
 #include "setup.h"
@@ -347,6 +347,9 @@ int Curl_read(struct connectdata *conn,
       case SSL_ERROR_WANT_WRITE:
         /* if there's data pending, then we re-invoke SSL_read() */
         break;
+      default:
+        failf(conn->data, "SSL read error: %d", err);
+        return CURLE_READ_ERROR;
       }
     } while(loop);
     if(loop && SSL_pending(conn->ssl.handle))
