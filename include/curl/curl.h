5a43842b27066d755937797f1ccb021babe6e50e
bagder <bagder>
2003-05-09 07:42:47 +0000

    CURLOPT_FTP_USE_EPRT added

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: curl.h,v 1.193 2003-04-02 07:48:56 bagder Exp $
+ * $Id: curl.h,v 1.194 2003-05-09 07:42:47 bagder Exp $
  ***************************************************************************/
 
 #include <stdio.h>
@@ -624,6 +624,11 @@ typedef enum {
      and password to whatever host the server decides. */
   CINIT(UNRESTRICTED_AUTH, LONG, 105),
 
+  /* Specificly switch on or off the FTP engine's use of the EPRT command ( it
+     also disables the LPRT attempt). By default, those ones will always be
+     attempted before the good old traditional PORT command. */     
+  CINIT(FTP_USE_EPRT, LONG, 106),
+
   CURLOPT_LASTENTRY /* the last unused */
 } CURLoption;
 
@@ -814,8 +819,8 @@ CURLcode curl_global_init(long flags);
 void curl_global_cleanup(void);
 
 /* This is the version number */
-#define LIBCURL_VERSION "7.10.4"
-#define LIBCURL_VERSION_NUM 0x070a04
+#define LIBCURL_VERSION "7.10.5-pre2"
+#define LIBCURL_VERSION_NUM 0x070a05
 
 /* linked-list structure for the CURLOPT_QUOTE option (and other) */
 struct curl_slist {
