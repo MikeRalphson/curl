9b3d172c11e4152e14329af27c64f5d7dee49e89
bagder <bagder>
2003-06-10 09:42:22 +0000

    Pass the error stream pointer to the URL globber, so that it can report errors correctly to the user, if need be.
    Also fixed so that a missing ] in the globbing process no longer leads
    to core dump.

--- src/urlglob.h
+++ src/urlglob.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: urlglob.h,v 1.11 2003-01-16 21:08:13 bagder Exp $
+ * $Id: urlglob.h,v 1.12 2003-06-10 09:42:22 bagder Exp $
  ***************************************************************************/
 typedef enum {UPTSet=1,UPTCharRange,UPTNumRange} URLPatternType;
 
@@ -53,9 +53,9 @@ typedef struct {
   char beenhere;
 } URLGlob;
 
-int glob_url(URLGlob**, char*, int *);
-char* next_url(URLGlob*);
-char* match_url(char*, URLGlob *); 
+int glob_url(URLGlob**, char*, int *, FILE *);
+char* glob_next_url(URLGlob*);
+char* glob_match_url(char*, URLGlob *); 
 void glob_cleanup(URLGlob* glob);
 
 #endif
