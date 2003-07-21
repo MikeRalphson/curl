19384f06d79ce08a784ea644a9f69ae012e80a81
bagder <bagder>
2003-07-21 08:25:31 +0000

    pre4-commit

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: curl.h,v 1.203 2003-07-04 16:29:23 bagder Exp $
+ * $Id: curl.h,v 1.204 2003-07-21 08:25:31 bagder Exp $
  ***************************************************************************/
 
 /* If you have problems, all libcurl docs and details are found here:
@@ -29,7 +29,7 @@
 
 /* This is the version number of the libcurl package from which this header
    file origins: */
-#define LIBCURL_VERSION "7.10.6-pre3"
+#define LIBCURL_VERSION "7.10.6-pre4"
 
 /* This is the numeric version of the libcurl version number, meant for easier
    parsing and comparions by programs. The LIBCURL_VERSION_NUM define will
