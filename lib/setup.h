71a7e9d6d04f5dddc56b07627bf35edc3f328b89
bagder <bagder>
2003-08-14 22:42:18 +0000

    Possible code for large file support, added within #if 0 so far.

--- lib/setup.h
+++ lib/setup.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: setup.h,v 1.29 2003-08-05 14:40:59 bagder Exp $
+ * $Id: setup.h,v 1.30 2003-08-14 22:42:18 bagder Exp $
  ***************************************************************************/
 
 /* MN 06/07/02 */
@@ -193,6 +193,21 @@ typedef struct hostent Curl_addrinfo;
 typedef struct in_addr Curl_ipconnect;
 #endif
 
+#if 0
+#if (SIZEOF_OFF_T > 4)
+/* off_t is bigger than 4 bytes, and that makes it our prefered variable
+   type for filesizes */
+typedef off_t filesize_t;
+#else
+#ifdef HAVE_LONGLONG
+/* we have long long, use this for filesizes internally */
+typedef long long filesize_t;
+#else
+/* small off_t and no long long, no support for large files :-( */
+typedef long filesize_t;
+#endif /* didn't have long long */
+#endif /* sizeof wasn't bigger than 4 */
 
+#endif /* 0 */
 
 #endif /* __CONFIG_H */
