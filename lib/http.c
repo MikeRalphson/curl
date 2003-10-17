dff7493c75935f7a1e57daa4a0d0448fcf255d05
bagder <bagder>
2003-10-17 09:29:30 +0000

    make empty names look fine in verbose output

--- lib/http.c
+++ lib/http.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: http.c,v 1.164 2003-10-17 07:05:26 bagder Exp $
+ * $Id: http.c,v 1.165 2003-10-17 09:29:30 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
@@ -238,7 +238,7 @@ CURLcode http_auth_headers(struct connectdata *conn,
         Curl_http_auth_stage(data, 401);
       }
       infof(data, "Proxy auth using %s with user '%s'\n",
-            auth, conn->proxyuser);
+            auth, conn->proxyuser?conn->proxyuser:"");
     }
     /* Send web authentication header if needed */
     if (data->state.authstage == 401) {
@@ -287,7 +287,7 @@ CURLcode http_auth_headers(struct connectdata *conn,
       }
       if(auth)
         infof(data, "Server auth using %s with user '%s'\n",
-              auth, conn->user);
+              auth, conn->user?conn->user:"");
     }
   }
   else
