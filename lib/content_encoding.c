0d4acd398fee222b2f23b70fe20b7644b9ccd41c
bagder <bagder>
2004-01-05 22:54:45 +0000

    Dan Fandrich's zlib fix

--- lib/content_encoding.c
+++ lib/content_encoding.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: content_encoding.c,v 1.7 2003-04-22 22:32:02 bagder Exp $
+ * $Id: content_encoding.c,v 1.8 2004-01-05 22:54:53 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
@@ -82,6 +82,8 @@ Curl_unencode_deflate_write(struct SessionHandle *data,
     z->zalloc = (alloc_func)Z_NULL;
     z->zfree = (free_func)Z_NULL;
     z->opaque = 0;              /* of dubious use 08/27/02 jhrg */
+    z->next_in = NULL;
+    z->avail_in = 0;
     if (inflateInit(z) != Z_OK)
       return process_zlib_error(data, z);
     k->zlib_init = 1;
@@ -224,6 +226,8 @@ Curl_unencode_gzip_write(struct SessionHandle *data,
     z->zalloc = (alloc_func)Z_NULL;
     z->zfree = (free_func)Z_NULL;
     z->opaque = 0;              /* of dubious use 08/27/02 jhrg */
+    z->next_in = NULL;
+    z->avail_in = 0;
     if (inflateInit2(z, -MAX_WBITS) != Z_OK)
       return process_zlib_error(data, z);
     k->zlib_init = 1;   /* Initial call state */
