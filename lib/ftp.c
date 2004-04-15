5efc0970cb4be8c93aecccadc41e551a113ea637
bagder <bagder>
2004-04-15 10:43:40 +0000

    ftp->dirs[] is no longer terminated with a zero entry but ftp->dirdepth should be used

--- lib/ftp.c
+++ lib/ftp.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: ftp.c,v 1.247 2004-04-15 07:52:40 bagder Exp $
+ * $Id: ftp.c,v 1.248 2004-04-15 10:43:40 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
@@ -2165,7 +2165,7 @@ CURLcode ftp_perform(struct connectdata *conn,
 
   {
     int i; /* counter for loop */
-    for (i=0; ftp->dirs[i]; i++) {
+    for (i=0; i < ftp->dirdepth; i++) {
       /* RFC 1738 says empty components should be respected too, but
          that is plain stupid since CWD can't be used with an empty argument */
       if ((result = ftp_cwd_and_mkd(conn, ftp->dirs[i])) != CURLE_OK)
