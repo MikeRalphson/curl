2c90f7976bde6a875bbd2dbf5e45d5b39212db52
bagder <bagder>
2004-04-15 07:52:39 +0000

    removed the fixed dir depth limit in the FTP code

--- lib/urldata.h
+++ lib/urldata.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: urldata.h,v 1.213 2004-04-13 07:37:28 bagder Exp $
+ * $Id: urldata.h,v 1.214 2004-04-15 07:52:40 bagder Exp $
  ***************************************************************************/
 
 /* This file is for lib internal stuff */
@@ -106,9 +106,6 @@
 /* Initial size of the buffer to store headers in, it'll be enlarged in case
    of need. */
 #define HEADERSIZE 256
-
-/* Maximum number of dirs supported by libcurl in a FTP dir hierarchy */
-#define CURL_MAX_FTP_DIRDEPTH 100
 
 /* Just a convenience macro to get the larger value out of two given.
    We prefix with CURL to prevent name collisions. */
@@ -244,7 +241,9 @@ struct FTP {
   char *user;    /* user name string */
   char *passwd;  /* password string */
   char *urlpath; /* the originally given path part of the URL */
-  char *dirs[CURL_MAX_FTP_DIRDEPTH]; /* path components */
+  char **dirs;   /* realloc()ed array for path components */
+  int dirdepth;  /* number of entries used in the 'dirs' array */
+  int diralloc;  /* number of entries allocated for the 'dirs' array */
   char *file;    /* decoded file */
 
   char *entrypath; /* the PWD reply when we logged on */
