b808ad25aebefdd211edde064f4f1aa0c758bd2e
bagder <bagder>
2004-05-24 11:57:34 +0000

    delete trailing whitespace

--- lib/curlx.h
+++ lib/curlx.h
@@ -1,10 +1,10 @@
 #ifndef __CURLX_H
 #define __CURLX_H
 /***************************************************************************
- *                                  _   _ ____  _     
- *  Project                     ___| | | |  _ \| |    
- *                             / __| | | | |_) | |    
- *                            | (__| |_| |  _ <| |___ 
+ *                                  _   _ ____  _
+ *  Project                     ___| | | |  _ \| |
+ *                             / __| | | | |_) | |
+ *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
  * Copyright (C) 1998 - 2004, Daniel Stenberg, <daniel@haxx.se>, et al.
@@ -12,7 +12,7 @@
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
  * are also available at http://curl.haxx.se/docs/copyright.html.
- * 
+ *
  * You may opt to use, copy, modify, merge, publish, distribute and/or sell
  * copies of the Software, and permit persons to whom the Software is
  * furnished to do so, under the terms of the COPYING file.
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: curlx.h,v 1.1 2004-04-30 08:22:38 bagder Exp $
+ * $Id: curlx.h,v 1.2 2004-05-24 11:57:34 bagder Exp $
  ***************************************************************************/
 
 /*
