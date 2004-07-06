fdb9f01770e8885df5a2015cbd98504232e5a44b
gknauf <gknauf>
2004-07-06 02:37:52 +0000

    fixed Win32 prebuild section; fixed minor cosmetic bug.

--- tests/testcurl.pl
+++ tests/testcurl.pl
@@ -19,7 +19,7 @@
 # This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
 # KIND, either express or implied.
 #
-# $Id: testcurl.pl,v 1.13 2004-07-05 23:35:49 gknauf Exp $
+# $Id: testcurl.pl,v 1.14 2004-07-06 02:37:52 gknauf Exp $
 ###########################################################################
 
 ###########################
@@ -55,7 +55,7 @@ use vars qw($version $fixed $infixed $CURLDIR $CVS $pwd $build $buildlog
 use vars qw($name $email $desc $confopts $setupfile $mktarball);
 
 # version of this script
-$version='$Revision: 1.13 $';
+$version='$Revision: 1.14 $';
 $fixed=0;
 
 # Determine if we're running from CVS or a canned copy of curl,
@@ -345,11 +345,11 @@ if ($gnulikebuild) {
     mydie "configure didn't work";
   }
 } else {
-  if ($^O eq 'MSWin32') {
+  if (($^O eq 'MSWin32') && ($targetos !~ /netware/)) {
     system("xcopy /s /q ..\\$CURLDIR .");
     system("buildconf.bat");
-  } elsif ($^O eq 'linux') {
-    system("cp -ar ../$CURLDIR/* ."); 
+  } elsif (($^O eq 'linux') || ($targetos =~ /netware/)) {
+    system("cp -afr ../$CURLDIR/* ."); 
     system("cp -af ../$CURLDIR/Makefile.dist Makefile"); 
     system("make -i -C lib -f Makefile.$targetos prebuild");
     system("make -i -C src -f Makefile.$targetos prebuild");
@@ -387,9 +387,9 @@ if (grepfile("define USE_ARES", "lib/config$confsuffix.h")) {
   close(F);
 
   if (-f "libcares$libext") {
-    logit "ares is now built successfully (libcares.$libext)";
+    logit "ares is now built successfully (libcares$libext)";
   } else {
-    logit "ares build failed (libares.$libext)";
+    logit "ares build failed (libares$libext)";
   }
 
   # cd back to the curl build dir
@@ -419,6 +419,12 @@ if ($gnulikebuild) {
     print;
   }
   close(F);
+}
+
+if (-f "lib/libcurl$libext") {
+  logit "lib/libcurl was created fine (libcurl$libext)";
+} else {
+  logit "lib/libcurl was not created (libcurl$libext)";
 }
 
 if (-f "src/curl$binext") {
