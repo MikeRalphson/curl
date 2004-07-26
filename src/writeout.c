d005db290dbd3b4b4de4369ec95490cd45db0842
bagder <bagder>
2004-07-26 09:09:26 +0000

    removed trailing whitespace

--- src/writeout.c
+++ src/writeout.c
@@ -1,8 +1,8 @@
 /***************************************************************************
- *                                  _   _ ____  _     
- *  Project                     ___| | | |  _ \| |    
- *                             / __| | | | |_) | |    
- *                            | (__| |_| |  _ <| |___ 
+ *                                  _   _ ____  _
+ *  Project                     ___| | | |  _ \| |
+ *                             / __| | | | |_) | |
+ *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
  * Copyright (C) 1998 - 2004, Daniel Stenberg, <daniel@haxx.se>, et al.
@@ -10,7 +10,7 @@
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
  * are also available at http://curl.haxx.se/docs/copyright.html.
- * 
+ *
  * You may opt to use, copy, modify, merge, publish, distribute and/or sell
  * copies of the Software, and permit persons to whom the Software is
  * furnished to do so, under the terms of the COPYING file.
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: writeout.c,v 1.22 2004-05-05 09:20:08 bagder Exp $
+ * $Id: writeout.c,v 1.23 2004-07-26 09:09:26 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
@@ -223,5 +223,5 @@ void ourWriteOut(CURL *curl, char *writeinfo)
       ptr++;
     }
   }
-  
+
 }
