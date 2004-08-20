8116412718a1948059ba17fad650ff295cd240ed
bagder <bagder>
2004-08-20 09:18:12 +0000

    actually, we check for a numerical host using either ipv4 or ipv6, as neither should result in a reverse dns lookup

--- lib/hostip6.c
+++ lib/hostip6.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: hostip6.c,v 1.8 2004-08-20 09:11:52 bagder Exp $
+ * $Id: hostip6.c,v 1.9 2004-08-20 09:18:12 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
@@ -247,7 +247,8 @@ Curl_addrinfo *Curl_getaddrinfo(struct connectdata *conn,
     }
   }
 
-  if(1 == inet_pton(pf, hostname, addrbuf)) {
+  if((1 == inet_pton(AF_INET, hostname, addrbuf)) ||
+     (1 == inet_pton(AF_INET6, hostname, addrbuf))) {
     /* the given address is numerical only, prevent a reverse lookup */
     ai_flags = AI_NUMERICHOST;
   }
