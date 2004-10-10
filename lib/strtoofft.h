644ffa7544b20b1af74db3099c1fcedd1ec3b9cf
bagder <bagder>
2004-10-10 03:39:59 +0000

    If long is 8 bytes we can use strtol() to get 64 bit numbers and won't need our strtoll() replacement function.

--- lib/strtoofft.h
+++ lib/strtoofft.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: strtoofft.h,v 1.11 2004-10-10 03:28:51 bagder Exp $
+ * $Id: strtoofft.h,v 1.12 2004-10-10 03:39:59 bagder Exp $
  ***************************************************************************/
 
 /*
@@ -38,7 +38,7 @@
  * not, should try to emulate its functionality.  At any rate, we define
  * 'strtoofft' such that it can be used to work with curl_off_t's regardless.
  */
-#if SIZEOF_CURL_OFF_T > 4
+#if (SIZEOF_CURL_OFF_T > 4) && (SIZEOF_LONG < 8)
 #if HAVE_STRTOLL
 #define curlx_strtoofft strtoll
 #else /* HAVE_STRTOLL */
@@ -53,8 +53,8 @@ curl_off_t curlx_strtoll(const char *nptr, char **endptr, int base);
 #endif /* MSVC7 or later */
 
 #endif /* HAVE_STRTOLL */
-#else /* SIZEOF_CURL_OFF_T > 4 */
-/* simply use strtol() to get 32bit numbers */
+#else /* (SIZEOF_CURL_OFF_T > 4) && (SIZEOF_LONG < 8) */
+/* simply use strtol() to get numbers, either 32 or 64 bit */
 #define curlx_strtoofft strtol
 #endif
 
