0af5f0033c23c154f4d3562abbf361597bf0f2ce
bagder <bagder>
2004-12-06 23:04:30 +0000

    Fixed so that the final error message is sent to the verbose info "stream" even if no errorbuffer is set.

--- lib/sendf.c
+++ lib/sendf.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: sendf.c,v 1.92 2004-10-12 12:49:38 bagder Exp $
+ * $Id: sendf.c,v 1.93 2004-12-06 23:04:31 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
@@ -158,21 +158,20 @@ void Curl_failf(struct SessionHandle *data, const char *fmt, ...)
   if(data->set.errorbuffer && !data->state.errorbuf) {
     vsnprintf(data->set.errorbuffer, CURL_ERROR_SIZE, fmt, ap);
     data->state.errorbuf = TRUE; /* wrote error string */
-
-    if(data->set.verbose) {
-      size_t len = strlen(data->set.errorbuffer);
-      bool doneit=FALSE;
-      if(len < CURL_ERROR_SIZE - 1) {
-        doneit = TRUE;
-        data->set.errorbuffer[len] = '\n';
-        data->set.errorbuffer[++len] = '\0';
-      }
-      Curl_debug(data, CURLINFO_TEXT, data->set.errorbuffer, len, NULL);
-      if(doneit)
-        /* cut off the newline again */
-        data->set.errorbuffer[--len]=0;
-    }
   }
+  if(data->set.verbose) {
+      size_t len;
+
+      vsnprintf(data->state.buffer, BUFSIZE, fmt, ap);
+      len = strlen(data->state.buffer);
+
+      if(len < BUFSIZE - 1) {
+        data->state.buffer[len] = '\n';
+        data->state.buffer[++len] = '\0';
+      }
+      Curl_debug(data, CURLINFO_TEXT, data->state.buffer, len, NULL);
+  }
+
   va_end(ap);
 }
 
