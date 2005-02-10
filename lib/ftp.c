958c4e85cabb42eb2e2354e173077290351f109e
bagder <bagder>
2005-02-10 07:45:26 +0000

    typecast assign to ftpport from int to prevent warnings

--- lib/ftp.c
+++ lib/ftp.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: ftp.c,v 1.301 2005-02-09 14:01:15 giva Exp $
+ * $Id: ftp.c,v 1.302 2005-02-10 07:45:26 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
@@ -1677,7 +1677,7 @@ static CURLcode ftp_state_port_resp(struct connectdata *conn,
 {
   struct FTP *ftp = conn->proto.ftp;
   struct SessionHandle *data = conn->data;
-  ftpport fcmd = ftp->count1;
+  ftpport fcmd = (ftpport)ftp->count1;
   CURLcode result = CURLE_OK;
 
   if(ftpcode != 200) {
