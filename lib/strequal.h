e15b0e26a516b879917002bab90463ef07235d39
bagder <bagder>
2005-04-12 07:56:57 +0000

    Provides an unconditional strlcat() proto even if strlcat() was found by configure. An attempt to fix warnings when we build and the strlcat() function is provided by one if the libs (gss or krb4) since then we have no protos for it in a system header.

--- lib/strequal.h
+++ lib/strequal.h
@@ -20,18 +20,10 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: strequal.h,v 1.15 2004-11-09 14:00:56 giva Exp $
+ * $Id: strequal.h,v 1.16 2005-04-12 07:56:57 bagder Exp $
  ***************************************************************************/
 
 #include <curl/curl.h>
-
-#if 0
-/*
- * These two actually are public functions, which are in <curl/curl.h>
- */
-int curl_strequal(const char *first, const char *second);
-int curl_strnequal(const char *first, const char *second, size_t max);
-#endif
 
 #define strequal(a,b) curl_strequal(a,b)
 #define strnequal(a,b,c) curl_strnequal(a,b,c)
@@ -45,7 +37,7 @@ char *Curl_strcasestr(const char *haystack, const char *needle);
 
 #ifndef HAVE_STRLCAT
 #define strlcat(x,y,z) Curl_strlcat(x,y,z)
-size_t Curl_strlcat(char *dst, const char *src, size_t siz);
 #endif
+size_t strlcat(char *dst, const char *src, size_t siz);
 
 #endif
