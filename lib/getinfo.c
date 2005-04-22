42b55070d8e225f27dee243aed64e6ea4580be0f
bagder <bagder>
2005-04-22 20:48:07 +0000

    modified this year

--- lib/getinfo.c
+++ lib/getinfo.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2004, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2005, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: getinfo.c,v 1.44 2005-04-07 15:27:14 bagder Exp $
+ * $Id: getinfo.c,v 1.45 2005-04-22 20:48:07 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
