0078483f56d437212851af30c242f965f1b5722e
bagder <bagder>
2005-05-10 11:19:12 +0000

    allow the ares/config.h display to fail

--- tests/testcurl.pl
+++ tests/testcurl.pl
@@ -19,7 +19,7 @@
 # This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
 # KIND, either express or implied.
 #
-# $Id: testcurl.pl,v 1.31 2005-05-09 07:45:30 bagder Exp $
+# $Id: testcurl.pl,v 1.32 2005-05-10 11:19:12 bagder Exp $
 ###########################################################################
 
 ###########################
@@ -67,7 +67,7 @@ use vars qw($name $email $desc $confopts $runtestopts $setupfile $mktarball
             $nocvsup $crosscompile);
 
 # version of this script
-$version='$Revision: 1.31 $';
+$version='$Revision: 1.32 $';
 $fixed=0;
 
 # Determine if we're running from CVS or a canned copy of curl,
@@ -439,11 +439,12 @@ if (grepfile("define USE_ARES", "lib/config$confsuffix.h")) {
   logit "setup to build ares";
 
   logit "display ares/config$confsuffix.h";
-  open(F, "ares/config$confsuffix.h") or die "ares/config$confsuffix.h: $!";
-  while (<F>) {
-      print if /^ *#/;
+  if(open(F, "ares/config$confsuffix.h")) {
+      while (<F>) {
+          print if /^ *#/;
+      }
+      close(F);
   }
-  close(F);
 
   logit "build ares";
   chdir "ares";
