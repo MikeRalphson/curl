855b18bffec9aee2ded00599dcf122e9dd9abc95
bagder <bagder>
2005-08-10 22:57:14 +0000

    the debug callback was called with CURLINFO_TEXT with the data size one too big

--- lib/ssluse.c
+++ lib/ssluse.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: ssluse.c,v 1.134 2005-04-13 06:52:03 bagder Exp $
+ * $Id: ssluse.c,v 1.135 2005-08-10 22:57:14 bagder Exp $
  ***************************************************************************/
 
 /*
@@ -1098,8 +1098,8 @@ static void ssl_tls_trace(int direction, int ssl_ver, int content_type,
   msg_type = *(char*)buf;
   msg_name = ssl_msg_type(ssl_ver, msg_type);
 
-  txt_len = 1 + snprintf(ssl_buf, sizeof(ssl_buf), "SSLv%c, %s%s (%d):\n",
-                         ver, tls_rt_name, msg_name, msg_type);
+  txt_len = snprintf(ssl_buf, sizeof(ssl_buf), "SSLv%c, %s%s (%d):\n",
+                     ver, tls_rt_name, msg_name, msg_type);
   Curl_debug(data, CURLINFO_TEXT, ssl_buf, txt_len, NULL);
 
   Curl_debug(data, (direction == 1) ? CURLINFO_SSL_DATA_OUT :
