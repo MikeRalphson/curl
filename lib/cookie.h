b76e8b9d050ddaf68276db4abfeed0b1a12c5d0c
bagder <bagder>
2005-08-17 08:55:43 +0000

    - Jeff Pohlmeyer found out that if you ask libcurl to load a cookiefile (with   CURLOPT_COOKIEFILE), add a cookie (with CURLOPT_COOKIELIST), tell it to   write the result to a given cookie jar and then never actually call   curl_easy_perform() - the given file(s) to read was never read but the   output file was written and thus it caused a "funny" result.
    - While doing some tests for the bug above, I noticed that Firefox generates
      large numbers (for the expire time) in the cookies.txt file and libcurl
      didn't treat them properly. Now it does.

--- lib/cookie.h
+++ lib/cookie.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: cookie.h,v 1.18 2005-07-27 22:17:15 bagder Exp $
+ * $Id: cookie.h,v 1.19 2005-08-17 08:55:43 bagder Exp $
  ***************************************************************************/
 
 #include <stdio.h>
@@ -38,7 +38,7 @@ struct Cookie {
   char *value;       /* name = <this> */
   char *path;         /* path = <this> */
   char *domain;      /* domain = <this> */
-  long expires;    /* expires = <this> */
+  curl_off_t expires;  /* expires = <this> */
   char *expirestr;   /* the plain text version */
   bool tailmatch;    /* weather we do tail-matchning of the domain name */
 
@@ -94,8 +94,10 @@ int Curl_cookie_output(struct CookieInfo *, char *);
 
 #if defined(CURL_DISABLE_HTTP) || defined(CURL_DISABLE_COOKIES)
 #define Curl_cookie_list(x) NULL
+#define Curl_cookie_loadfiles(x)
 #else
 struct curl_slist *Curl_cookie_list(struct SessionHandle *data);
+void Curl_cookie_loadfiles(struct SessionHandle *data);
 #endif
 
 #endif
