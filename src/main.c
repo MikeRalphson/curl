29d49094cc1fbf972edcc6eb8b115dfd7a618d12
bagder <bagder>
2005-10-27 12:05:36 +0000

    Jaz Fresh pointed out that if you used "-r [number]" as was wrongly described in the man page, curl would send an invalid HTTP Range: header. The correct way would be to use "-r [number]-" or even "-r -[number]". Starting now, curl will warn if this is discovered, and automatically append a dash to the range before passing it to libcurl.

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: main.c,v 1.333 2005-10-20 21:19:19 bagder Exp $
+ * $Id: main.c,v 1.334 2005-10-27 12:05:37 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
@@ -2097,8 +2097,24 @@ static ParameterError getparameter(char *flag, /* f or -long-flag */
         return err;
       break;
     case 'r':
-      /* byte range requested */
-      GetStr(&config->range, nextarg);
+      /* Specifying a range WITHOUT A DASH will create an illegal HTTP range
+         (and won't actually be range by definition). The man page previously
+         claimed that to be a good way, why this code is added to work-around
+         it. */
+      if(!strchr(nextarg, '-')) {
+        char buffer[32];
+        curl_off_t off;
+        warnf(config,
+              "A specfied range MUST include at least one dash (-). "
+              "Appending one for you!\n");
+        off = curlx_strtoofft(nextarg, NULL, 10);
+        snprintf(buffer, sizeof(buffer), CURL_FORMAT_OFF_T "-", off);
+        GetStr(&config->range, buffer);
+      }
+      else
+        /* byte range requested */
+        GetStr(&config->range, nextarg);
+
       break;
     case 'R':
       /* use remote file's time */
