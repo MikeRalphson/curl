cff049746be9794ac19ab64fe4cf9fb54db48d6b
bagder <bagder>
2006-02-23 21:33:02 +0000

    Fixed typo, the option is called --write-out. Bob Bagwill pointed out.

--- src/writeout.c
+++ src/writeout.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2005, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2006, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: writeout.c,v 1.28 2005-09-06 13:27:18 bagder Exp $
+ * $Id: writeout.c,v 1.29 2006-02-23 21:33:02 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
@@ -220,7 +220,7 @@ void ourWriteOut(CURL *curl, char *writeinfo)
             }
           }
           if(!match) {
-            fprintf(stderr, "curl: unknown --writeout variable: '%s'\n", ptr);
+            fprintf(stderr, "curl: unknown --write-out variable: '%s'\n", ptr);
           }
           ptr=end+1; /* pass the end */
           *end = keepit;
