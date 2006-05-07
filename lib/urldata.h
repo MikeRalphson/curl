906b5eb7b8693d9fc9b945ae0011353bacb24e2b
bagder <bagder>
2006-05-07 18:27:36 +0000

    Fix GnuTLS compile warning. Risking breakage with some older version of GnuTLS?

--- lib/urldata.h
+++ lib/urldata.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: urldata.h,v 1.289 2006-04-26 13:08:12 bagder Exp $
+ * $Id: urldata.h,v 1.290 2006-05-07 18:27:36 bagder Exp $
  ***************************************************************************/
 
 /* This file is for lib internal stuff */
@@ -158,7 +158,7 @@ struct ssl_connect_data {
 #endif /* USE_SSLEAY */
 #ifdef USE_GNUTLS
   gnutls_session session;
-  gnutls_anon_client_credentials cred;
+  gnutls_certificate_credentials cred;
 #endif /* USE_GNUTLS */
 };
 
