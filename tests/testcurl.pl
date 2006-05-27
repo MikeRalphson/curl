a0aab086086f4ff76fc757b05d9cc91ababdcbf8
bagder <bagder>
2006-05-27 11:36:05 +0000

    reverted the previous .libs check for libs, but instead make mingw32 builds more similar to other configure builds (== use .la for libext and no binext)

--- tests/testcurl.pl
+++ tests/testcurl.pl
@@ -19,7 +19,7 @@
 # This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
 # KIND, either express or implied.
 #
-# $Id: testcurl.pl,v 1.39 2006-05-25 11:04:08 bagder Exp $
+# $Id: testcurl.pl,v 1.40 2006-05-27 11:36:05 bagder Exp $
 ###########################################################################
 
 ###########################
@@ -68,7 +68,7 @@ use vars qw($name $email $desc $confopts $runtestopts $setupfile $mktarball
             $nocvsup $nobuildconf $crosscompile);
 
 # version of this script
-$version='$Revision: 1.39 $';
+$version='$Revision: 1.40 $';
 $fixed=0;
 
 # Determine if we're running from CVS or a canned copy of curl,
@@ -129,10 +129,9 @@ if ($^O eq 'MSWin32' || $targetos) {
     # If no target defined on Win32 lets assume vc
     $targetos = 'vc';
   }
-  if ($targetos =~ /vc/ || $targetos =~ /mingw32/ || $targetos =~ /borland/) {
+  if ($targetos =~ /vc/ || $targetos =~ /borland/) {
     $binext = '.exe';
-    $libext = '.lib' if ($targetos =~ /vc/ || $targetos =~ /borland/);
-    $libext = '.a' if ($targetos =~ /mingw32/);
+    $libext = '.lib';
   }
   elsif ($targetos =~ /netware/) {
     $configurebuild = 0;
@@ -141,7 +140,8 @@ if ($^O eq 'MSWin32' || $targetos) {
   }
 }
 
-if (($^O eq 'MSWin32') && ($targetos =~ /vc/ || $targetos =~ /mingw32/ || $targetos =~ /borland/)) {
+if (($^O eq 'MSWin32') &&
+    ($targetos =~ /vc/ || $targetos =~ /mingw32/ || $targetos =~ /borland/)) {
 
   # Set these things only when building ON Windows and for Win32 platform.
   # FOR Windows since we might be cross-compiling on another system. Non-
@@ -496,7 +496,7 @@ if (grepfile("define USE_ARES", "lib/config$confsuffix.h")) {
   }
   close(F);
 
-  if (-f "libcares$libext" || -f ".libs/libcares$libext") {
+  if (-f "libcares$libext") {
     logit "ares is now built successfully (libcares$libext)";
   } else {
     logit "ares build failed (libares$libext)";
@@ -535,7 +535,7 @@ else {
   close(F);
 }
 
-if (-f "lib/libcurl$libext" || -f "lib/.libs/libcurl$libext") {
+if (-f "lib/libcurl$libext") {
   logit "libcurl was created fine (libcurl$libext)";
 }
 else {
