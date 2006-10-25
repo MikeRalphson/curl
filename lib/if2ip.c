bf247e465937ad1d705a923d1d9e7605c7e0d956
bagder <bagder>
2006-10-25 07:19:45 +0000

    updated copyright year

--- lib/if2ip.c
+++ lib/if2ip.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2005, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2006, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: if2ip.c,v 1.48 2006-08-16 18:48:27 danf Exp $
+ * $Id: if2ip.c,v 1.49 2006-10-25 07:19:45 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
--- lib/llist.c
+++ lib/llist.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2005, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2006, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: llist.c,v 1.17 2006-09-07 21:49:22 bagder Exp $
+ * $Id: llist.c,v 1.18 2006-10-25 07:19:45 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
