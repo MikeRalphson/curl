940cd7f39c19f265b9f2a58f96f06586b4572e11
giva <giva>
2006-10-30 16:26:24 +0000

    Allow 'curl_*printf()' to be used in C++ programs.

--- include/curl/mprintf.h
+++ include/curl/mprintf.h
@@ -20,13 +20,17 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: mprintf.h,v 1.13 2006-03-28 10:08:54 bagder Exp $
+ * $Id: mprintf.h,v 1.14 2006-10-30 16:26:24 giva Exp $
  ***************************************************************************/
 
 #include <stdarg.h>
 #include <stdio.h> /* needed for FILE */
 
 #include "curl.h"
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
 
 CURL_EXTERN int curl_mprintf(const char *format, ...);
 CURL_EXTERN int curl_mfprintf(FILE *fd, const char *format, ...);
@@ -57,6 +61,10 @@ CURL_EXTERN char *curl_mvaprintf(const char *format, va_list args);
 # define vsnprintf curl_mvsnprintf
 # define aprintf curl_maprintf
 # define vaprintf curl_mvaprintf
+#endif
+
+#ifdef  __cplusplus
+}
 #endif
 
 #endif /* __CURL_MPRINTF_H */
