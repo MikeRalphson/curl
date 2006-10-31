f56faeddef14ce332c1ec16efa10036cd0f278cf
giva <giva>
2006-10-31 17:54:51 +0000

    Added Watt-32 section to fix things for Watt32+Win32 targets.

--- ares/config-win32.h
+++ ares/config-win32.h
@@ -1,7 +1,7 @@
 #ifndef __ARES_CONFIG_WIN32_H
 #define __ARES_CONFIG_WIN32_H
 
-/* $Id: config-win32.h,v 1.9 2006-10-14 12:01:44 yangtse Exp $ */
+/* $Id: config-win32.h,v 1.10 2006-10-31 17:54:51 giva Exp $ */
 
 /* Copyright (C) 2004 - 2005 by Daniel Stenberg et al
  *
@@ -126,6 +126,16 @@
 
 /* Define to the function return type for send. */
 #define SEND_TYPE_RETV int
+
+/* Specifics for the Watt-32 tcp/ip stack */
+#ifdef WATT32
+  #define SOCKET              int
+  #define NS_INADDRSZ         4
+  #define HAVE_ARPA_NAMESER_H 1
+  #undef HAVE_WINSOCK_H
+  #undef HAVE_WINSOCK2_H
+  #undef HAVE_WS2TCPIP_H
+#endif
 
 /* ---------------------------------------------------------------- */
 /*                          STRUCT RELATED                          */
