58a2619aab435b17fd0ccec24ba014453cf443a8
bagder <bagder>
2006-11-11 22:23:48 +0000

    we did 1.3.2 and are now on the 1.3.3 track!

--- ares/ares_version.h
+++ ares/ares_version.h
@@ -1,15 +1,15 @@
-/* $Id: ares_version.h,v 1.6 2006-08-29 15:17:48 bagder Exp $ */
+/* $Id: ares_version.h,v 1.7 2006-11-11 22:23:48 bagder Exp $ */
 
 #ifndef ARES__VERSION_H
 #define ARES__VERSION_H
 
 #define ARES_VERSION_MAJOR 1
 #define ARES_VERSION_MINOR 3
-#define ARES_VERSION_PATCH 1
+#define ARES_VERSION_PATCH 3
 #define ARES_VERSION ((ARES_VERSION_MAJOR<<16)|\
                        (ARES_VERSION_MINOR<<8)|\
                        (ARES_VERSION_PATCH))
-#define ARES_VERSION_STR "1.3.1"
+#define ARES_VERSION_STR "1.3.3-CVS"
 
 #ifdef  __cplusplus
 extern "C" {
