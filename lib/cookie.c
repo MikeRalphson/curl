cab38b7c4e05b08ab561dff66779928af3fadcca
bagder <bagder>
2007-01-13 23:32:14 +0000

    make Curl_cookie_clearall() survive getting called with a NULL pointer

--- lib/cookie.c
+++ lib/cookie.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2006, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2007, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: cookie.c,v 1.76 2006-09-27 21:00:45 bagder Exp $
+ * $Id: cookie.c,v 1.77 2007-01-13 23:32:14 bagder Exp $
  ***************************************************************************/
 
 /***
@@ -806,9 +806,11 @@ struct Cookie *Curl_cookie_getlist(struct CookieInfo *c,
  ****************************************************************************/
 void Curl_cookie_clearall(struct CookieInfo *cookies)
 {
-  Curl_cookie_freelist(cookies->cookies);
-  cookies->cookies = NULL;
-  cookies->numcookies = 0;
+  if(cookies) {
+    Curl_cookie_freelist(cookies->cookies);
+    cookies->cookies = NULL;
+    cookies->numcookies = 0;
+  }
 }
 
 /*****************************************************************************
