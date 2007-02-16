028c554368be0c7b50230f8a1e8139809dc69c8d
bagder <bagder>
2007-02-16 15:27:49 +0000

    maxdownload is actually -1 for unlimited

--- lib/urldata.h
+++ lib/urldata.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: urldata.h,v 1.321 2007-02-13 22:50:16 bagder Exp $
+ * $Id: urldata.h,v 1.322 2007-02-16 15:27:49 bagder Exp $
  ***************************************************************************/
 
 /* This file is for lib internal stuff */
@@ -656,7 +656,7 @@ struct HandleData {
   curl_off_t size;        /* -1 if unknown at this point */
   curl_off_t *bytecountp; /* return number of bytes read or NULL */
 
-  curl_off_t maxdownload; /* in bytes, the maximum amount of data to fetch, 0
+  curl_off_t maxdownload; /* in bytes, the maximum amount of data to fetch, -1
                              means unlimited */
   curl_off_t *writebytecountp; /* return number of bytes written or NULL */
 
