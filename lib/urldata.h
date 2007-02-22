b3c8d058f9809000da8bae59e0b520d5f98983ad
yangtse <yangtse>
2007-02-22 06:19:39 +0000

    Fix compiler warnings
    "case label value exceeds maximum value for type" and
    "comparison is always false due to limited range of data type"
    
    Both triggered when using a bool variable as the switch variable
    in a switch statement and using enums for the case targets.

--- lib/urldata.h
+++ lib/urldata.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: urldata.h,v 1.324 2007-02-20 22:02:11 linus Exp $
+ * $Id: urldata.h,v 1.325 2007-02-22 06:19:39 yangtse Exp $
  ***************************************************************************/
 
 /* This file is for lib internal stuff */
@@ -521,6 +521,16 @@ struct hostname {
 #define KEEP_WRITE_HOLD 8 /* when set, no writing should be done but there
                              might still be data to write */
 
+#ifdef HAVE_LIBZ
+typedef enum {
+  ZLIB_UNINIT,          /* uninitialized */
+  ZLIB_INIT,            /* initialized */
+  ZLIB_GZIP_HEADER,     /* reading gzip header */
+  ZLIB_GZIP_INFLATING,  /* inflating gzip stream */
+  ZLIB_INIT_GZIP        /* initialized in transparent gzip mode */
+} zlibInitState;
+#endif
+
 /*
  * This struct is all the previously local variables from Curl_perform() moved
  * to struct to allow the function to return and get re-invoked better without
@@ -581,7 +591,7 @@ struct Curl_transfer_keeper {
 #define COMPRESS 3              /* Not handled, added for completeness */
 
 #ifdef HAVE_LIBZ
-  bool zlib_init;               /* True if zlib already initialized;
+  zlibInitState zlib_init;      /* possible zlib init state;
                                    undefined if Content-Encoding header. */
   z_stream z;                   /* State structure for zlib. */
 #endif
