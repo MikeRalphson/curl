0a65cc22855848009e99913b073661d23686fd99
danf <danf>
2007-03-22 19:45:20 +0000

    Free some additional strings on exit to avoid memory leaks.

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: main.c,v 1.399 2007-03-09 21:51:44 bagder Exp $
+ * $Id: main.c,v 1.400 2007-03-22 19:45:20 danf Exp $
  ***************************************************************************/
 #include "setup.h"
 
@@ -3229,8 +3229,12 @@ static void free_config_fields(struct Configurable *config)
     free(config->writeout);
   if(config->httppost)
     curl_formfree(config->httppost);
+  if (config->cert)
+    free(config->cert);
   if(config->cacert)
     free(config->cacert);
+  if (config->cert_type)
+    free(config->cert_type);
   if(config->capath)
     free(config->capath);
   if(config->cookiejar)
@@ -3245,6 +3249,14 @@ static void free_config_fields(struct Configurable *config)
     free(config->socksproxy);
   if(config->libcurl)
     free(config->libcurl);
+  if (config->key_passwd)
+    free(config->key_passwd);
+  if (config->key)
+    free(config->key);
+  if (config->key_type)
+    free(config->key_type);
+  if (config->referer)
+    free(config->referer);
 
   curl_slist_free_all(config->quote); /* checks for config->quote == NULL */
   curl_slist_free_all(config->prequote);
