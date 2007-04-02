7fb7219296042ceb97978a9ceb6f9e6109a44c78
yangtse <yangtse>
2007-04-02 04:14:59 +0000

    fix error in previous commit

--- tests/sshserver.pl
+++ tests/sshserver.pl
@@ -1,5 +1,5 @@
 #/usr/bin/env perl
-# $Id: sshserver.pl,v 1.9 2007-04-02 01:21:57 yangtse Exp $
+# $Id: sshserver.pl,v 1.10 2007-04-02 04:14:59 yangtse Exp $
 # Start sshd for use in the SCP and SFTP curl test harness tests
 
 # Options:
@@ -105,7 +105,7 @@ if ($verbose) {
 }
 
 # Verify minimum OpenSSH version.
-if ($ssh_daemon !~ /OpenSSH/) || (10 * $ssh_ver_major + $ssh_ver_minor < 37)
+if (($ssh_daemon !~ /OpenSSH/) || (10 * $ssh_ver_major + $ssh_ver_minor < 37)) {
     print "SCP and SFTP tests require OpenSSH 3.7 or later\n";
     exit 1;
 }
