915a1e5225f3a2b945872d7df71d40dd162f862e
bagder <bagder>
2007-06-11 13:32:49 +0000

    Properly wait for the c-ares resolve to complete, hopefully the cure for bug #1733955

--- lib/hostares.c
+++ lib/hostares.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: hostares.c,v 1.30 2007-05-31 11:34:32 bagder Exp $
+ * $Id: hostares.c,v 1.31 2007-06-11 13:32:49 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
@@ -253,8 +253,9 @@ CURLcode Curl_wait_for_resolv(struct connectdata *conn,
 
     tvp = ares_timeout(data->state.areschannel, &store, &tv);
 
-    if(!ares_waitperform(conn, tv.tv_sec * 1000 + tv.tv_usec/1000))
-      /* no sockets to wait on, get out of the loop */
+    ares_waitperform(conn, timeout);
+
+    if(conn->async.done)
       break;
 
     timediff = Curl_tvdiff(Curl_tvnow(), now); /* spent time */
