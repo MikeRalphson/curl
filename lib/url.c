6d6b9d39932c42c51394d28e09ff99e51cd81d8c
danf <danf>
2007-08-07 18:24:49 +0000

    Fixed torture test for test 509

--- lib/url.c
+++ lib/url.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: url.c,v 1.638 2007-08-07 00:10:28 danf Exp $
+ * $Id: url.c,v 1.639 2007-08-07 18:24:49 danf Exp $
  ***************************************************************************/
 
 /* -- WIN32 approved -- */
@@ -1318,7 +1318,8 @@ CURLcode Curl_setopt(struct SessionHandle *data, CURLoption option,
     result = Curl_setstropt(&data->set.str[STRING_SET_URL],
                             va_arg(param, char *));
     data->change.url = data->set.str[STRING_SET_URL];
-    data->change.url_changed = TRUE;
+    if (data->change.url)
+      data->change.url_changed = TRUE;
     break;
   case CURLOPT_PORT:
     /*
