4d21463394dfe8f7dc6090aa8d3749ac1549e59e
bagder <bagder>
2007-09-28 20:28:06 +0000

    Bumped version to 1.5.0 for next release and soname bumped to 2 due to ABI and API changes in the progress callback (and possibly more coming up from Steinar)

--- ares/ares_version.h
+++ ares/ares_version.h
@@ -1,15 +1,15 @@
-/* $Id: ares_version.h,v 1.9 2007-06-08 09:01:33 bagder Exp $ */
+/* $Id: ares_version.h,v 1.10 2007-09-28 20:28:06 bagder Exp $ */
 
 #ifndef ARES__VERSION_H
 #define ARES__VERSION_H
 
 #define ARES_VERSION_MAJOR 1
-#define ARES_VERSION_MINOR 4
-#define ARES_VERSION_PATCH 1
+#define ARES_VERSION_MINOR 5
+#define ARES_VERSION_PATCH 0
 #define ARES_VERSION ((ARES_VERSION_MAJOR<<16)|\
                        (ARES_VERSION_MINOR<<8)|\
                        (ARES_VERSION_PATCH))
-#define ARES_VERSION_STR "1.4.1-CVS"
+#define ARES_VERSION_STR "1.5.0-CVS"
 
 #ifdef  __cplusplus
 extern "C" {
