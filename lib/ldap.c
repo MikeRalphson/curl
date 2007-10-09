c0b37b89d32c4e964b58b9b848d011149512efd1
gknauf <gknauf>
2007-10-09 23:25:58 +0000

    added check for MSVC6 standard PSDK and bail out since insufficient for LDAP support with current code.

--- lib/ldap.c
+++ lib/ldap.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: ldap.c,v 1.84 2007-10-02 14:48:38 yangtse Exp $
+ * $Id: ldap.c,v 1.85 2007-10-09 23:25:58 gknauf Exp $
  ***************************************************************************/
 
 #include "setup.h"
@@ -45,7 +45,11 @@
 
 #ifdef CURL_LDAP_WIN            /* Use W$ LDAP implementation. */
 # include <winldap.h>
-# include <winber.h>
+# ifndef LDAP_VENDOR_NAME
+#  error Your Platform SDK is NOT sufficient for LDAP support! Update your Platform SDK, or disable LDAP LDAP support!
+# else
+#  include <winber.h>
+# endif
 #else
 #define LDAP_DEPRECATED 1       /* Be sure ldap_init() is defined. */
 # include <ldap.h>
