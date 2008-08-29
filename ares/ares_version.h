9fba287b99543b4e8dad71f853725e5d490b1de1
bagder <bagder>
2008-08-29 08:55:02 +0000

    we start over working towards 1.5.4

--- ares/ares_version.h
+++ ares/ares_version.h
@@ -1,15 +1,15 @@
-/* $Id: ares_version.h,v 1.13 2008-05-29 20:39:18 bagder Exp $ */
+/* $Id: ares_version.h,v 1.14 2008-08-29 08:55:02 bagder Exp $ */
 
 #ifndef ARES__VERSION_H
 #define ARES__VERSION_H
 
 #define ARES_VERSION_MAJOR 1
 #define ARES_VERSION_MINOR 5
-#define ARES_VERSION_PATCH 3
+#define ARES_VERSION_PATCH 4
 #define ARES_VERSION ((ARES_VERSION_MAJOR<<16)|\
                        (ARES_VERSION_MINOR<<8)|\
                        (ARES_VERSION_PATCH))
-#define ARES_VERSION_STR "1.5.3-CVS"
+#define ARES_VERSION_STR "1.5.4-CVS"
 
 #ifdef  __cplusplus
 extern "C" {
