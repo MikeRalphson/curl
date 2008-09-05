cc4f5ea19c006149baed275860ebfc2ae71bdcd4
danf <danf>
2008-09-05 17:58:53 +0000

    Improved the logic the decides whether to use HTTP 1.1 features or not in a request.
    Detect cases where an upload must be sent chunked and the server supports
    only HTTP 1.0 and return CURLE_UPLOAD_FAILED.

--- lib/url.c
+++ lib/url.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: url.c,v 1.737 2008-09-05 16:13:21 bagder Exp $
+ * $Id: url.c,v 1.738 2008-09-05 17:58:59 danf Exp $
  ***************************************************************************/
 
 /* -- WIN32 approved -- */
@@ -4689,7 +4689,8 @@ CURLcode Curl_done(struct connectdata **connp,
  * do_init() inits the readwrite session. This is inited each time (in the DO
  * function before the protocol-specific DO functions are invoked) for a
  * transfer, sometimes multiple times on the same SessionHandle. Make sure
- * nothing in here depends on stuff that are setup dynamicly for the transfer.
+ * nothing in here depends on stuff that are setup dynamically for the
+ * transfer.
  */
 
 static CURLcode do_init(struct connectdata *conn)
@@ -4706,7 +4707,6 @@ static CURLcode do_init(struct connectdata *conn)
   k->start = Curl_tvnow(); /* start time */
   k->now = k->start;   /* current time is now */
   k->header = TRUE; /* assume header */
-  k->httpversion = -1; /* unknown at this point */
 
   k->bytecount = 0;
 
