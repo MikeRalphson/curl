367feaa1cc09e5e29000a77ba46ddbc01ca47be8
danf <danf>
2008-09-15 22:35:09 +0000

    Changed the test data file so it passes an XML syntax check

--- tests/libtest/lib506.c
+++ tests/libtest/lib506.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * $Id: lib506.c,v 1.19 2008-07-15 05:46:50 yangtse Exp $
+ * $Id: lib506.c,v 1.20 2008-09-15 22:35:09 danf Exp $
  */
 
 #include "test.h"
@@ -54,7 +54,7 @@ static void my_lock(CURL *handle, curl_lock_data data, curl_lock_access laccess,
       fprintf(stderr, "lock: no such data: %d\n", (int)data);
       return;
   }
-  printf("lock:   %-6s <%s>: %d\n", what, user->text, user->counter);
+  printf("lock:   %-6s [%s]: %d\n", what, user->text, user->counter);
   user->counter++;
 }
 
@@ -78,7 +78,7 @@ static void my_unlock(CURL *handle, curl_lock_data data, void *useptr )
       fprintf(stderr, "unlock: no such data: %d\n", (int)data);
       return;
   }
-  printf("unlock: %-6s <%s>: %d\n", what, user->text, user->counter);
+  printf("unlock: %-6s [%s]: %d\n", what, user->text, user->counter);
   user->counter++;
 }
 
