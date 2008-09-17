ff95a400a21f46b25b0d4139e26c9a3b39729b7b
danf <danf>
2008-09-17 17:33:23 +0000

    Removed reference to curl-ca-bundle.crt in the host verification failure error message.

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: main.c,v 1.486 2008-09-13 03:55:21 yangtse Exp $
+ * $Id: main.c,v 1.487 2008-09-17 17:33:24 danf Exp $
  ***************************************************************************/
 #include "setup.h"
 
@@ -4942,8 +4942,8 @@ show_error:
 #define CURL_CA_CERT_ERRORMSG1 \
 "More details here: http://curl.haxx.se/docs/sslcerts.html\n\n" \
 "curl performs SSL certificate verification by default, using a \"bundle\"\n" \
-" of Certificate Authority (CA) public keys (CA certs). The default\n" \
-" bundle is named curl-ca-bundle.crt; you can specify an alternate file\n" \
+" of Certificate Authority (CA) public keys (CA certs). If the default\n" \
+" bundle file isn't adequate, you can specify an alternate file\n" \
 " using the --cacert option.\n"
 
 #define CURL_CA_CERT_ERRORMSG2 \
