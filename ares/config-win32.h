d74410dd9f083ed4cc9c23df09efe5aefeaa71ad
yangtse <yangtse>
2008-11-01 16:52:57 +0000

    WIN32 availability of freeaddrinfo, getaddrinfo and getnameinfo functions is quite convoluted, compiler dependant and in some cases even build target dependat.

--- ares/config-win32.h
+++ ares/config-win32.h
@@ -1,7 +1,7 @@
 #ifndef __ARES_CONFIG_WIN32_H
 #define __ARES_CONFIG_WIN32_H
 
-/* $Id: config-win32.h,v 1.23 2008-09-15 15:28:26 yangtse Exp $ */
+/* $Id: config-win32.h,v 1.24 2008-11-01 16:52:57 yangtse Exp $ */
 
 /* Copyright (C) 2004 - 2008 by Daniel Stenberg et al
  *
@@ -219,6 +219,20 @@
 #  endif
 #  if (_WIN32_WINNT < 0x0501) || (WINVER < 0x0501)
 #    error VS2008 does not support Windows build targets prior to WinXP
+#  endif
+#endif
+
+/* Availability of freeaddrinfo, getaddrinfo and getnameinfo functions is quite */
+/* convoluted, compiler dependant and in some cases even build target dependat. */
+#if defined(HAVE_WS2TCPIP_H)
+#  if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#    define HAVE_FREEADDRINFO 1
+#    define HAVE_GETADDRINFO  1
+#    define HAVE_GETNAMEINFO  1
+#  elif defined(__MINGW32__) && defined(_WIN32_WINNT) && (_WIN32_WINNT >= 0x0501)
+#    define HAVE_FREEADDRINFO 1
+#    define HAVE_GETADDRINFO  1
+#    define HAVE_GETNAMEINFO  1
 #  endif
 #endif
 
