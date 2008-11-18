d9288b6c477654c9773ab00459f53498ad2c52fe
danf <danf>
2008-11-18 09:11:34 +0000

    Made an array static const

--- lib/gtls.c
+++ lib/gtls.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: gtls.c,v 1.52 2008-11-18 08:53:51 danf Exp $
+ * $Id: gtls.c,v 1.53 2008-11-18 09:11:34 danf Exp $
  ***************************************************************************/
 
 /*
@@ -345,7 +345,7 @@ Curl_gtls_connect(struct connectdata *conn,
     return CURLE_SSL_CONNECT_ERROR;
 
   if(data->set.ssl.version == CURL_SSLVERSION_SSLv3) {
-    int protocol_priority[] = { GNUTLS_SSL3, 0 };
+    static const int protocol_priority[] = { GNUTLS_SSL3, 0 };
     gnutls_protocol_set_priority(session, protocol_priority);
     if(rc < 0)
       return CURLE_SSL_CONNECT_ERROR;
