989d97adba108f5ed46d6e9572d83a4650713ffb
bagder <bagder>
2009-02-14 09:12:55 +0000

    nah, use the simpler year - year range syntax only, no matter what emacs' copyright-update script thinks

--- lib/content_encoding.c
+++ lib/content_encoding.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2007, 2009, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2009, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: content_encoding.c,v 1.29 2009-02-14 09:09:10 bagder Exp $
+ * $Id: content_encoding.c,v 1.30 2009-02-14 09:12:55 bagder Exp $
  ***************************************************************************/
 
 #include "setup.h"
