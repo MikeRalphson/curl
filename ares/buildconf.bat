3a2940f97193e650a4b9015915e56c0c2bb3ad27
yangtse <yangtse>
2009-04-30 10:43:27 +0000

    buildconf.bat for CVS-tree c-ares

---
+++ ares/buildconf.bat
@@ -0,0 +1,8 @@
+@echo off
+REM set up a CVS tree to build when there's no autotools
+REM $Revision: 1.1 $
+REM $Date: 2009-04-30 10:43:27 $
+
+REM create ares_build.h
+copy /Y ares_build.h.dist ares_build.h
+
--- buildconf.bat
+++ buildconf.bat
@@ -1,7 +1,7 @@
 @echo off
 REM set up a CVS tree to build when there's no autotools
-REM $Revision: 1.6 $
-REM $Date: 2008-08-07 00:29:08 $
+REM $Revision: 1.7 $
+REM $Date: 2009-04-30 10:43:27 $
 
 REM create hugehelp.c
 copy src\hugehelp.c.cvs src\hugehelp.c
@@ -11,3 +11,11 @@ copy Makefile.dist Makefile
 
 REM create curlbuild.h
 copy include\curl\curlbuild.h.dist include\curl\curlbuild.h
+
+REM setup c-ares CVS tree
+if not exist ares\buildconf.bat goto end_c_ares
+cd ares
+call buildconf.bat
+cd ..
+:end_c_ares
+
