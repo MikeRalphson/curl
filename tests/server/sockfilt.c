4a10243dff6bf77d1b3b91aeea3a98746e8f1e32
yangtse <yangtse>
2009-05-14 19:52:19 +0000

    Add some debug tracing

--- tests/server/sockfilt.c
+++ tests/server/sockfilt.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: sockfilt.c,v 1.61 2009-05-14 13:00:54 yangtse Exp $
+ * $Id: sockfilt.c,v 1.62 2009-05-14 19:52:19 yangtse Exp $
  ***************************************************************************/
 
 /* Purpose
@@ -765,11 +765,13 @@ static curl_socket_t sockdaemon(curl_socket_t sock,
 #endif
       la_size = sizeof(localaddr4);
       localaddr = (struct sockaddr *)&localaddr4;
+      logmsg("debug: IPv4 la_size = %zu\n", (size_t)la_size);
 #ifdef ENABLE_IPV6
     }
     else {
       la_size = sizeof(localaddr6);
       localaddr = (struct sockaddr *)&localaddr6;
+      logmsg("debug: IPv6 la_size = %zu\n", (size_t)la_size);
     }
 #endif
     memset(localaddr, 0, (size_t)la_size);
@@ -780,16 +782,20 @@ static curl_socket_t sockdaemon(curl_socket_t sock,
       sclose(sock);
       return CURL_SOCKET_BAD;
     }
+    logmsg("debug: localaddr->sa_family = %d\n", (int)localaddr->sa_family);
     switch (localaddr->sa_family) {
     case AF_INET:
+      logmsg("debug: AF_INET localaddr4.sin_port = %hu\n", localaddr4.sin_port);
       *listenport = ntohs(localaddr4.sin_port);
       break;
 #ifdef ENABLE_IPV6
     case AF_INET6:
+      logmsg("debug: AF_INET6 localaddr6.sin6_port = %hu\n", localaddr6.sin6_port);
       *listenport = ntohs(localaddr6.sin6_port);
       break;
 #endif
     default:
+      logmsg("debug: AF_UNKNOWN\n");
       break;
     }
     if(!*listenport) {
