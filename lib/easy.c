159dd404bfb42a064c5623c7a078d89d5f746a41
gknauf <gknauf>
2009-05-17 18:22:39 +0000

    fixed case.

--- lib/easy.c
+++ lib/easy.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: easy.c,v 1.139 2009-05-17 17:40:30 yangtse Exp $
+ * $Id: easy.c,v 1.140 2009-05-17 18:22:39 gknauf Exp $
  ***************************************************************************/
 
 #include "setup.h"
@@ -281,7 +281,7 @@ CURLcode curl_global_init(long flags)
 #endif
 
 #if defined(USE_ARES) && defined(ARES_VERSION) && (ARES_VERSION >= 0x010601)
-  If(ares_library_init(ARES_LIB_INIT_ALL)) {
+  if(ares_library_init(ARES_LIB_INIT_ALL)) {
     DEBUGF(fprintf(stderr, "Error: ares_library_init failed\n"));
     return CURLE_FAILED_INIT;
   }
