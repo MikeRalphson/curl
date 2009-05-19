87a855c16485686615436dc3af7638da18533f94
yangtse <yangtse>
2009-05-19 15:23:25 +0000

    Add same copyright notice as other c-ares files

--- ares/ares_library_init.c
+++ ares/ares_library_init.c
@@ -1,4 +1,20 @@
-/* $Id: ares_library_init.c,v 1.4 2009-05-19 15:19:03 yangtse Exp $ */
+/* $Id: ares_library_init.c,v 1.5 2009-05-19 15:23:25 yangtse Exp $ */
+
+/* Copyright 1998 by the Massachusetts Institute of Technology.
+ * Copyright (C) 2004-2009 by Daniel Stenberg
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in
+ * advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
 
 #include "setup.h"
 
--- ares/ares_library_init.h
+++ ares/ares_library_init.h
@@ -1,7 +1,23 @@
 #ifndef HEADER_CARES_LIBRARY_INIT_H
 #define HEADER_CARES_LIBRARY_INIT_H
 
-/* $Id: ares_library_init.h,v 1.1 2009-05-17 17:10:41 yangtse Exp $ */
+/* $Id: ares_library_init.h,v 1.2 2009-05-19 15:23:25 yangtse Exp $ */
+
+/* Copyright 1998 by the Massachusetts Institute of Technology.
+ * Copyright (C) 2004-2009 by Daniel Stenberg
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in
+ * advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
 
 #include "setup.h"
 
