38e0bdc1a2648c88ead7341e4d584703c77ab566
bagder <bagder>
2009-06-08 21:25:16 +0000

    - Claes Jakobsson provided a patch for libcurl-NSS that fixed a bad refcount   issue with client certs that caused issues like segfaults.   http://curl.haxx.se/mail/lib-2009-05/0316.html

--- lib/urldata.h
+++ lib/urldata.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: urldata.h,v 1.412 2009-05-11 07:53:38 bagder Exp $
+ * $Id: urldata.h,v 1.413 2009-06-08 21:25:17 bagder Exp $
  ***************************************************************************/
 
 /* This file is for lib internal stuff */
@@ -211,7 +211,6 @@ struct ssl_connect_data {
 #ifdef USE_NSS
   PRFileDesc *handle;
   char *client_nickname;
-  CERTCertificate *client_cert;
 #ifdef HAVE_PK11_CREATEGENERICOBJECT
   PK11GenericObject *key;
   PK11GenericObject *cacert[2];
